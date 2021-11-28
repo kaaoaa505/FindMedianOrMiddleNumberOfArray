8a585c72010465bcf18fd3ce15b5d5016975032d
Khaled <kaaoaa505@gmail.com>
2021-11-28 07:12:47 +0200

    find median or middle number of array

---
+++ README.md
@@ -0,0 +1,17 @@
+Given a sorted array of integers a,
+
+find median/middle value.
+
+**Example**
+
+For a = [2, 4, 7], the output should be
+solve(a) = 4.
+
+For a = [2, 4, 7, 6], the output should be
+solve(a) = 4.
+
+For a = [2, 4, 7, 6, 6], the output should be
+solve(a) = 7.
+
+For a = [2, 4, 7, 6, 6, 8], the output should be
+solve(a) = 7.
---
+++ src/index.ts
@@ -0,0 +1,27 @@
+function solve(a: number[] = []): number {
+    let x: number = 0;
+    const isEven: boolean = a.length % 2 === 0;
+
+    return isEven ? a[Math.floor(a.length / 2) - 1] : a[Math.floor(a.length / 2)];
+}
+
+const a01 = [2, 4, 7];
+console.log(solve(a01)); // 4
+
+console.log('________________________________');
+
+const a02 = [2, 4, 7, 6];
+console.log(solve(a02)); // 4
+
+console.log('________________________________');
+
+const a03 = [2, 4, 7, 6, 6];
+console.log(solve(a03)); // 7
+
+console.log('________________________________');
+
+const a04 = [2, 4, 7, 6, 6, 8];
+console.log(solve(a04)); // 7
+
+console.log('________________________________');
+
